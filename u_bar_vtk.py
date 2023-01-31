3578b498c23e92b4b94e8ee0f8778ab79ad5881a
Raphael Muenster <raphael.muenster@gmail.com>
2023-01-31 15:09:21 +0100

    Improved parallelization for u_bar_vtk.py

--- u_bar_vtk.py
+++ u_bar_vtk.py
@@ -1,17 +1,17 @@
+"""
+This script computes the average velocity of a given .vtu file series
+"""
 import os
 import re
 import sys
 import glob
+import argparse
 import numpy as np
 import xml.etree.ElementTree as ET
-#for i in piece_???.00000.dat; do cat $i >> all_00000.data; done
-#for((i=0;i<=10000;i=i+50)); do for((j=1;j<=48;j++)); do name=$(printf "res_node_%03d.%05d.vtu" $j $i); echo $name; done; done
-#for((i=0;i<=1000;i=i+50)); do for((j=1;j<=48;j++)); do name=$(printf "res_node_%03d.%05d.vtu" $j $i); python3 ./parse_vtk.py "_vtk/$name"; done; done
-#1000
-#for((i=0;i<=1000;i=i+50)); do for((j=1;j<=48;j++)); do name1=$(printf "piece_%03d.%05d.dat" $j $i); name2=$(printf "step_%05d.res" $i); echo $name1; cat $name1 >> $name2; done; done
-#_vtk/res_node_000.
+
 #~/anaconda3/python.exe u_avg_vtk.py files.dat 2 1 27
 #for i in {1..27}; do ~/anaconda3/python.exe partial_sums.py $i 3; done
+
 def parseVtu(fileName):
     tree = ET.parse(fileName)
     root = tree.getroot()
@@ -33,11 +33,6 @@ def parseVtu(fileName):
         
     v = np.array(vel)
     return v
-
-def writeParsed(vel, press, fileName):
-    with open(fileName, "w") as f:
-        for idx, item in enumerate(press):
-            f.write("%s %s %s %s\n" %(press[idx], vel[idx][0], vel[idx][1], vel[idx][2]))
 
 def processFiles(res, steps, procs):
 
@@ -83,18 +78,119 @@ def processFileList(fileName, off, steps, procs):
         partId = "%d-%d" %(off,off+steps-1)
         np.savetxt("u_avg_%03d_part_%s.txt" %(j, partId), u_avg)
 
-def main():
-    # args: offset step 
-    if len(sys.argv) <= 1:
-        print(sys.argv)
-        files = r'd:\work\avg\main.*.pvtu'
-        res = glob.glob(files)
-        steps = len(res)
-        processFiles(res, steps, 2)
-    else:
-        print("here")
-        processFileList(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]))
+def extractFileNumber(files):
+    fileList = []
+    for file in files:
+        fileBase = os.path.basename(file)
+        fileName, ext = os.path.splitext(fileBase)
+        dirName = os.path.dirname(file)
+        idx = fileName.split('.')[1]
+        fileList.append(idx)
+    return fileList
 
+def calcSize(files, idx):
+    pidx = idx
+    if len(files) >= 1:
+        file = files[0]
+        fileBase = os.path.basename(file)
+        fileName, ext = os.path.splitext(fileBase)
+        dirName = os.path.dirname(file)
+        fidx = fileName.split('.')[1]
+
+        name1 = "res_node_%03d.%s.vtu" %(pidx, fidx)
+        name = os.path.join(dirName, name1)
+        u_bar = parseVtu(name)
+        return u_bar.shape
+
+def processListProcs(files, idxStart, idxEnd):
+
+    fileNumbers = extractFileNumber(files)
+    partId = "%s-%s" %(fileNumbers[0],fileNumbers[len(files)-1])
+    for pidx in range(idxStart, idxEnd+1):
+        size = calcSize(files, pidx)
+        u_bar = np.zeros(size)
+        for file in files:
+            fileBase = os.path.basename(file)
+            fileName, ext = os.path.splitext(fileBase)
+            dirName = os.path.dirname(file)
+            fidx = fileName.split('.')[1]
+
+            name1 = "res_node_%03d.%s.vtu" %(pidx, fidx)
+            name = os.path.join(dirName, name1)
+            print(name)
+            u = parseVtu(name)
+            u_bar = u_bar + u
+
+        np.savetxt("u_avg_%03d_part_%s.txt" %(pidx, partId), u_bar)
+
+#            fileBase = os.path.basename(file)
+#            outDir = args.outputDir 
+#            outputName = "%s/c_piece_%s.dat" %(outDir, fileBase[9:])
+
+#        inputName = res[off].strip()
+#        name = "res_node_%03d.%s.vtu" %(j,inputName)
+#        print(name)
+#        u_avg = parseVtu(name)
+#        for i in range(off+1, off + steps):
+#            inputName = res[i].strip() 
+#            name = "res_node_%03d.%s.vtu" %(j,inputName)
+#            print(name)
+#            v = parseVtu(name)
+#            u_avg = u_avg + v
+##            print(len(v), v.shape)
+#
+#        partId = "%d-%d" %(off,off+steps-1)
+#        np.savetxt("u_avg_%03d_part_%s.txt" %(j, partId), u_avg)
+
+
+def main():
+    """
+    The script can get be started with different argument configurations:
+    - u_bar_vtk.py files.dat startLine #numFiles procsStart procsEnd
+    #====================================================================
+    files.dat: the total list of files to process
+    startLine: the line in files.dat from which to start
+    #numFiles: the number of files to process from the start
+    procsStart: the processor idx to start with
+    procsEnd: the processor idx to end with
+    """
+
+    # Set up the argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--filePattern", help="Pattern for the file series to process. Has to be put into \"\" on Bash, etc... to stop expansion.")
+    parser.add_argument("-p", "--procs", help="Here we specify which processor output to process: 1 -> processor 1 OR 3-5 processors 3 to 5")
+    args = parser.parse_args()
+
+    print(args.filePattern)
+    print(args.procs)
+    print(len(args.procs.split("-")))
+
+    if args.filePattern:
+        res = glob.glob(args.filePattern)
+        print(res)
+        #print(os.getcwd())
+    
+    if args.procs:
+        valProcs = args.procs.split("-")
+        procs = len(valProcs)
+        if procs > 1:
+            procsStart = int(valProcs[0])
+            procsEnd   = int(valProcs[1])
+
+    print("%d %d" %(procsStart, procsEnd))
+
+    processListProcs(res, procsStart, procsEnd)
+
+#    if len(sys.argv) <= 1:
+#        print(sys.argv)
+#        files = r'd:\work\avg\main.*.pvtu'
+#        res = glob.glob(files)
+#        steps = len(res)
+#        #processFiles(res, steps, 2)
+#    else:
+#        print("here")
+#        #processFileList(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]))
+    sys.exit(-2)
 #    print(res[0])
 #    for j in range(1, 2):
 #        inputName = res[0]
@@ -114,16 +210,6 @@ def main():
 ##            print(len(v), v.shape)
 #
 #        np.savetxt("u_avg_%03d.txt" %(j), u_avg)
-
-#    inputName = sys.argv[1]
-#    file, ext = os.path.splitext(inputName)
-#    outputName = "./sampleData/piece_%s.dat" %(file[13:])
-#    print(inputName)
-#    print(outputName)
-#    (v, p) = parseVtu(inputName)
-#    writeParsed(v, p, outputName)
-#    print(len(p))
-#    print(len(v))
 
 if __name__ == "__main__":
     main()
